60fffa90ff1996f43988e66dbc2220465f7da0b0
RSSST <136626663+RSSST@users.noreply.github.com>
2023-06-15 16:08:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,22 +4,12 @@
 # subprocess.check_call([sys.executable, "-m", "pip", "install", 'xgboost'])
 # subprocess.check_call([sys.executable, "-m", "pip", "install", 'scikit-learn'])
 
-import numpy as np
-import pandas as pd
 import joblib
 import streamlit as st
+import numpy as np
+import pandas as pd
 import xgboost as xgb
-
-
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
-from sklearn.model_selection import cross_val_score
-from sklearn.ensemble import RandomForestClassifier
-import xgboost as xgb
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.naive_bayes import GaussianNB
-from sklearn.model_selection import GridSearchCV
+from pandas import DataFrame, read_csv, Series
 
 model = joblib.load('XGB_model.joblib')
 st.title('Breast Cancer Prediction')
@@ -36,11 +26,10 @@ fractal_dimension_mean = st.number_input('Fractal dimension mean:')
 
 cols = ['radius_mean','texture_mean', 'perimeter_mean', 'area_mean', 'smoothness_mean', 'compactness_mean', 'concavity_mean', 'concave points_mean', 'symmetry_mean', 'fractal_dimension_mean']
 
-def predicted():
-    # row = np.array([radius_mean,texture_mean, perimeter_mean, area_mean, smoothness_mean, compactness_mean, concavity_mean, concavepoint_mean, symmetry_mean, fractal_dimension_mean])
-    row = np.array([20.57,17.77,132.9,1326,0.08474,0.07864,0.0869,0.07017,0.1812,0.05667])
-    g = pd.DataFrame([row], columns=cols)
-    prediction = model.predict(g)[0]
+def predict():
+    row = np.array([radius_mean,texture_mean, perimeter_mean, area_mean, smoothness_mean, compactness_mean, concavity_mean, concavepoint_mean, symmetry_mean, fractal_dimension_mean])
+    x = pd.DataFrame([row], columns=cols)
+    prediction = model.predict(x)[0]
 
     if prediction == 0:
         st.success('Low risk')
@@ -48,4 +37,5 @@ def predicted():
         st.error('High risk')
 
 
-st.button('Predict', on_click=predicted)
+st.button('Predict', on_click=predict)
+
