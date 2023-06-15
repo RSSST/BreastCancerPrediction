7b612100910ea2edf57b2c2bc8e2fbc16adf1ddf
RSSST <136626663+RSSST@users.noreply.github.com>
2023-06-15 15:29:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import subprocess
-import sys
-subprocess.check_call([sys.executable, "-m", "pip", "install", 'joblib'])
-subprocess.check_call([sys.executable, "-m", "pip", "install", 'xgboost'])
+# import subprocess
+# import sys
+# subprocess.check_call([sys.executable, "-m", "pip", "install", 'joblib'])
+# subprocess.check_call([sys.executable, "-m", "pip", "install", 'xgboost'])
 
 import numpy as np
 import pandas as pd
